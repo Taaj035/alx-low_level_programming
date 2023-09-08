a1d9df9653fb5108918c86ae8bc8750805f327b7
Taaj035 <tajjudeen@gmail.com>
2023-09-07 21:13:33 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ * Task 4-Puts
+*/
+#include <stdio.h>
+/**
+ * main - Prints "Programming is like building a mutlilingual puzzle, followed by new line"
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
