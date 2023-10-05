e66d46dc1d5ce7887de34b895af6198606942ce1
Taaj035 <tajjudeen@gmail.com>
2023-10-05 14:56:27 -0700

    1-strdup.c correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _strdup - function returns a pointer to a new string which is a duplicate of the string str.
+ * _strdup - a function that returns a pointer to a new string
  * @str: string to copy
  * Return : Ponter
  */
