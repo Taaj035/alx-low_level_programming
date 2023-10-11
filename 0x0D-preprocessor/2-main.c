cd708361320c546126ce6d2d96f35ecff5f68dfc
Taaj035 <tajjudeen@gmail.com>
2023-10-10 19:18:02 -0700

    File name correction

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - This program prints the name of the file it was compiled from, followed by a new line.
+ * main - prints the name of the file it was compiled from
  * @void: Null
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
